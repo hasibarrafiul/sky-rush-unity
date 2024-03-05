edde964541ffb3df06cfcf2218359f4098cd32bd
hasibarrafiul <fahad.com66.fk@gmail.com>
2024-03-06 04:40:24 +0900

    ui

--- Assets/Script/LoadingSceneManager.cs
+++ Assets/Script/LoadingSceneManager.cs
@@ -8,10 +8,18 @@ public class LoadingSceneManager : MonoBehaviour
     // Start is called before the first frame update
     public string targetSceneName;
     public float minimumLoadingTime = 2f; // Minimum loading time in seconds
+    public GameObject zeroPercentBar;
+    public GameObject twentyPercentBar;
+    public GameObject fortyPercentBar;
+    public GameObject sixtyPercentBar;
+    public GameObject eightyPercentBar;
+    public GameObject hundredPercentBar;
+
 
     void Start()
     {
         StartCoroutine(LoadSceneAsync());
+        zeroPercentBar.SetActive(true);
     }
 
     IEnumerator LoadSceneAsync()
@@ -24,11 +32,33 @@ public class LoadingSceneManager : MonoBehaviour
         while (!operation.isDone)
         {
             // Display loading progress (e.g., update progress bar)
-
+            print(operation.progress);
+            if (operation.progress >= 0.2f)
+            {
+                zeroPercentBar.SetActive(false);
+                twentyPercentBar.SetActive(true);
+            }
+            if (operation.progress >= 0.4f)
+            {
+                twentyPercentBar.SetActive(false);
+                fortyPercentBar.SetActive(true);
+            }
+            if (operation.progress >= 0.6f)
+            {
+                fortyPercentBar.SetActive(false);
+                sixtyPercentBar.SetActive(true);
+            }
+            if (operation.progress >= 0.8f)
+            {
+                sixtyPercentBar.SetActive(false);
+                eightyPercentBar.SetActive(true);
+            }
             if (operation.progress >= 0.9f)
             {
                 // When the loading progress is nearly complete
                 // (0.9f is considered fully loaded), allow scene activation
+                eightyPercentBar.SetActive(false);
+                hundredPercentBar.SetActive(true);
                 operation.allowSceneActivation = true;
             }
 
